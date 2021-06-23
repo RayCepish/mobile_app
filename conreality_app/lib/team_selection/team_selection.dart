bb4684fbf96c1ca11579632909be7e111caecbe0
krasutskui <miha.com.com.23@gmail.com>
2021-06-23 23:13:59 +0300

    adding sign up/login pages

---
+++ conreality_app/lib/team_selection/team_selection.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class TeamSelection extends StatefulWidget {
+  TeamSelection({Key? key}) : super(key: key);
+
+  @override
+  _TeamSelectionState createState() => _TeamSelectionState();
+}
+
+class _TeamSelectionState extends State<TeamSelection> {
+  final Map PeopleIcons = {
+    
+  };
+  Color caughtColor = Colors.grey;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [],
+    );
+  }
+}
